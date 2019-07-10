8a70a0f1b50cbd461d10ef0bfcd264d9eeff80f1
Mirko Bronzi <m.bronzi@gmail.com>
2019-07-10 15:43:26 -0400

    modified the sbatch example

---
+++ example_orion_on_cluster_with_slurm/to_submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --array=2
+#SBATCH --job-name=orion_test
+#SBATCH --output=out_%a.log
+#SBATCH --error=err_%a.log
+#SBATCH --account=rpp-bengioy
+#SBATCH --gres=gpu:1
+#SBATCH --time=00:05:00
+#SBATCH --mem=1Gb
+
+module load python/3.6
+source ../../ve/bin/activate
+orion -v hunt --config orion_config.yaml ../my_exp_code/main.py --config exp_config.yaml --saved_model '{exp.working_dir}/{exp.name}_{trial.id}/model.pt' --log '{exp.working_dir}/{exp.name}_{trial.id}/exp.log'
